296f9bb641a2d267df0fa04845a15d00b2a9dbcf
supriya-143 <muvvasupriya03@gmail.com>
2026-02-04 13:37:53 +0530

    Update source code file

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from quantum_optimizer import quantum_route_optimizer
 
 app = Flask(__name__)
 
-# 1️⃣ Root endpoint
+
 @app.route("/")
 def home():
     return "Hello DevOps Intern!"
 
-# 2️⃣ Health check endpoint
+
 @app.route("/health")
 def health():
     return "OK"
 
-# 3️⃣ Existing quantum optimization endpoint
+
 @app.route("/optimize-route", methods=["POST"])
 def optimize_route():
     data = request.get_json()
